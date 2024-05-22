a369ecb3b21683ba7e00aed21b7c74e73c0df2d6
Maeva COLLE <macolle>
2024-05-22 12:01:05 +0200

    creation

---
+++ Source/DouShouQi/DouShouQiLib/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DouShouQiLib
+{
+    internal class Manager
+    {
+        public Game Game { get; set; }
+        public Plateau Plateau { get; set; }    
+      
+        public IRegles Regles { get; set; }
+        public Joueur[] Joueur { get; set; } = new Joueur[2];
+      
+    }
+}
