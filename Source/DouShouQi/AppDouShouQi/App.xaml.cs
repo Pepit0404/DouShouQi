839a0b65ad0d1a9be72115c58998701c75f48f10
Maeva COLLE <macolle>
2024-05-23 19:34:13 +0200

    oui

--- Source/DouShouQi/AppDouShouQi/App.xaml.cs
+++ Source/DouShouQi/AppDouShouQi/App.xaml.cs
@@ -1,11 +1,14 @@
-﻿namespace AppDouShouQi
+﻿using DouShouQiLib;
+using static DouShouQiLib.Joueur;
+namespace AppDouShouQi
 {
+    
     public partial class App : Application
     {
+        public Manager TheMgr { get; set; }= new Manager();
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
     }
