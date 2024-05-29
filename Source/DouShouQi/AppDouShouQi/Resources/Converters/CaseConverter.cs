773b32010cf4d16afdf2c7bac25b6d574dd8ac2a
Erwan MECHOUD <ermechoud>
2024-05-29 12:12:03 +0200

    #7 tableau avec image

--- Source/DouShouQi/AppDouShouQi/Resources/Converters/CaseConverter.cs
+++ Source/DouShouQi/AppDouShouQi/Resources/Converters/CaseConverter.cs
@@ -8,20 +8,37 @@ public class CaseColorConverter : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         CaseType type = (CaseType)value!;
-        switch (type)
+        return type switch
         {
-            case CaseType.Terre:
-                return "Yellow";
-            case CaseType.Eau:
-                return "Blue";
-            case CaseType.Piege:
-                return "Red";
-            case CaseType.Taniere:
-                return "Green";
-            default: return "DarkSalmon";
-        }
+            CaseType.Terre => "Yellow",
+            CaseType.Eau => "Blue",
+            CaseType.Piege => "Red",
+            CaseType.Taniere => "Green",
+            _ => "DarkSalmon",
+        };
     }
 
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class CaseImageConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        CaseType type = (CaseType)value!;
+        return type switch
+        {
+            CaseType.Terre => "terre.jfif",
+            CaseType.Eau => "eau.jpg",
+            CaseType.Piege => "piege.jpg",
+            CaseType.Taniere => "taniere.jpg",
+            _ => null,
+        };
+    }
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
